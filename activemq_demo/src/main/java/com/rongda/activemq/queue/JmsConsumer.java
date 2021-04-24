d03ceaa9de0b117cfa7c126656b7a669e6de6567
rdlunc <348799577@qq.com>
2021-04-24 16:44:57 +0800

    消息消费者MessageListener方法说明

--- activemq_demo/src/main/java/com/rongda/activemq/queue/JmsConsumer.java
+++ activemq_demo/src/main/java/com/rongda/activemq/queue/JmsConsumer.java
@@ -1,19 +1,22 @@
 package com.rongda.activemq.queue;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.activemq.ActiveMQConnectionFactory;
 
 import javax.jms.*;
+import java.io.IOException;
 
 /**
  * @author lunrongda
  * @create 2021-04-13 23:25
  */
+@Slf4j
 public class JmsConsumer {
 
     public static final String ACTIVEMQ_URL = "tcp://192.168.1.106:61616";
     private static final String QUEUE_NAME = "queue01";
 
-    public static void main(String[] args) throws JMSException {
+    public static void main(String[] args) throws JMSException, IOException {
         //1.创建连接工厂，按照给定的url地址，采用默认用户名和密码
         ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory(ACTIVEMQ_URL);
         //2.通过连接工厂，获得连接connection并启动访问
@@ -28,6 +31,30 @@ public class JmsConsumer {
 
         //5.创建消费者
         MessageConsumer messageConsumer = session.createConsumer(queue);
+
+        //通过监听的方式来消费消息
+        messageConsumer.setMessageListener(new MessageListener() {
+            @Override
+            public void onMessage(Message message) {
+                if (message != null && message instanceof TextMessage){
+                    TextMessage textMessage = (TextMessage)message;
+                    try {
+                        log.info("*********》》》 消息者接收消息： " + textMessage.getText());
+                    } catch (JMSException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        System.in.read();
+        messageConsumer.close();
+        session.close();
+        connection.close();
+
+        /*
+        //同步阻塞方式 [receive()]
+        //订阅者或接收者调用 MessageConsumer 的 receive() 方法来接收消息，receive方法在能够接收到消息之前（或超时之前）将一直阻塞。
+
         while(true){
             //receive()方法是一直等待，“不见不散”。
             //TextMessage textMessage = (TextMessage)messageConsumer.receive();
@@ -36,7 +63,7 @@ public class JmsConsumer {
             TextMessage textMessage = (TextMessage)messageConsumer.receive(4000L);
 
             if (null != textMessage){
-                System.out.println("============> 消息者接收消息： " + textMessage.getText());
+                log.info("============> 消息者接收消息： " + textMessage.getText());
             } else {
                 break;
             }
@@ -44,6 +71,8 @@ public class JmsConsumer {
         messageConsumer.close();
         session.close();
         connection.close();
+        */
+
     }
 
 }
