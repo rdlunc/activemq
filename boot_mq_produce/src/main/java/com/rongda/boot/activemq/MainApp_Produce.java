d3303f20a3ae39716dd5e39c17f179a67780ddf0
rdlunc <348799577@qq.com>
2021-05-31 09:20:02 +0800

    SpringBoot整合ActiveMQ之队列生产者间隔定投

--- boot_mq_produce/src/main/java/com/rongda/boot/activemq/MainApp_Produce.java
+++ boot_mq_produce/src/main/java/com/rongda/boot/activemq/MainApp_Produce.java
@@ -2,12 +2,14 @@ package com.rongda.boot.activemq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author lunrongda
  * @create 2021-05-30 19:41
  */
 @SpringBootApplication
+@EnableScheduling
 public class MainApp_Produce {
 
     public static void main(String[] args) {
--- boot_mq_produce/src/main/java/com/rongda/boot/activemq/produce/Queue_Produce.java
+++ boot_mq_produce/src/main/java/com/rongda/boot/activemq/produce/Queue_Produce.java
@@ -3,6 +3,7 @@ package com.rongda.boot.activemq.produce;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsMessagingTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import javax.jms.Queue;
@@ -25,5 +26,14 @@ public class Queue_Produce {
     public void produceMsg(){
         jmsMessagingTemplate.convertAndSend(queue, "******: " + UUID.randomUUID().toString().substring(0,6));
         log.info("==========>>> produceMsg task is over.");
+    }
+
+    /**
+     * 间隔时间3秒钟定投
+     */
+    @Scheduled(fixedDelay = 3000)
+    public void produceMsgScheduled(){
+        jmsMessagingTemplate.convertAndSend(queue, "****** Scheduled: " + UUID.randomUUID().toString().substring(0,6));
+        log.info("==========>>> Scheduled produceMsg task is over.");
     }
 }
